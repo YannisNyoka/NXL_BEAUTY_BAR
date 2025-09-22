4e9df1db13ae938967c6d3a8d4c3d8806c78e626
yannis <yannisnyoka@gmail.com>
2025-09-22 12:24:58 +0200

    eye

--- FrontEnd/signup-ui/src/Home.jsx
+++ FrontEnd/signup-ui/src/Home.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react';
 import noxoloImage from './assets/images/nxl nails.jpg';
 import manicureImage from './assets/images/NxlPic5.jpg';
 import pedicureImage from './assets/images/ToesImage.jpg';
-import eyelashesImage from './assets/images/EyelashesImage.jpg';
+import eyelashesImage from './assets/images/eyelashesImage.jpg';
 
 
 
